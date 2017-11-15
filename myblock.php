4fd4eacede396f7e2eeb14004d4de8cd1e008280
Leonid <32931746+Leonid25@users.noreply.github.com>
2017-11-15 23:17:39 +0400

    Add files via upload

---
+++ myblock.php
@@ -0,0 +1,164 @@
+<?php
+?>
+<!Doctype html>
+<html>
+<head>
+	<style>
+  body { background: url(https://cdn.pixabay.com/photo/2017/03/14/16/57/silver-2143730__340.jpg); }
+</style>
+<div class="btn-group" role="group" aria-label="Basic example">
+  <button type="button" class="btn btn-danger">Главная</button>
+  <button type="button" class="btn btn-warning">Обо мне</button>
+</div>
+<body>
+<h1 style="color:#0000ff">Личный блок Леонида Шевкопляса</h1>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+	<title><?= $surname . '' . $name; ?></title>
+	<meta charset = "utf-8">
+	<style type = "text/css">
+	body{
+		font-family: sans-serif;
+	}
+
+	dl{
+		display: table-row;
+	}
+
+	dt, dd {
+		display: table-cell;
+		padding: 3px 8px;
+	}
+    </style>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+<div id="accordion" role="tablist">
+  <div class="card">
+    <div class="card-header" role="tab" id="headingOne">
+      <h5 class="mb-0">
+      	<h1 style="color:#CD5C5C">Осенние блюда</h1>
+        <a data-toggle="collapse" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+         <h4>Салат из крабовых палочек с кукурузой</h4>
+        </a>
+      </h5>
+    </div>
+
+    <div id="collapseOne" class="collapse show" role="tabpanel" aria-labelledby="headingOne" data-parent="#accordion">
+      <div class="card-body">
+        <p>КОНСЕРВИРОВАННАЯ КУКУРУЗА--------------------50 г</p>
+<p>КРАБОВЫЕ ПАЛОЧКИ-------------------------------------  50 г</p>
+<p>РИС--------------------------------------------2 столовые ложки</p>
+<p>ЯЙЦО КУРИНОЕ---------------------------------------- 1 штука</p>
+<p>ЗЕЛЕНЫЙ ЛУК------------------------------------------ по вкусу</p>
+<p>УКРОП----------------------------------------------------- по вкусу</p>
+<p>ПЕТРУШКА-----------------------------------------------по вкусу</p>
+<p>СОЛЬ-------------------------------------------------------по вкусу</p>
+<p>МАЙОНЕЗ------------------------------------------------по вкусу</p>
+<h1 style="color:#CD5C5C">Приготовление</h1>
+<p>1.Отварить рис и охладить. Яйца сварить вкрутую, очистить и мелко нарезать. Крабовые палочки порезать небольшими кусочками.</p>
+<p>2.Слить с кукурузы сок, смешать с крабовыми палочками, яйцами и рисом. Добавить мелко рубленную зелень.</p>
+<p>3.Посолить, заправить салат майонезом и перемешать.</p>
+<p><img src="https://img03.rl0.ru/eda/c620x415i/s2.eda.ru/StaticContent/Photos/120214122930/120214123648/p_O.jpg">
+
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-header" role="tab" id="headingTwo">
+      <h5 class="mb-0">
+        <a class="collapsed" data-toggle="collapse" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+          <h4>Гуляш c болгарским перцем</h4>
+        </a>
+      </h5>
+    </div>
+    <div id="collapseTwo" class="collapse" role="tabpanel" aria-labelledby="headingTwo" data-parent="#accordion">
+      <div class="card-body">
+        <p>Говядина------------------------------------- 1 Кг</p>
+<p>Луковица------------------------------------- 2 Штуки</p>
+<p>Чеснок---------------------------------------- 4 Зубчика</p>
+<p>Болгарский перец------------------------------------------ 2 Штуки</p>
+<p>Помидоры----------------------------------------------------- 2 Штуки</p>
+<p>Лавровый лист ---------------------------------------------- 2-3 Штуки</p>
+<p>Паприка красная сушеная --------------------------------- 1 Ст. ложка</p>
+<p>Растительное масло или жир ----------------------------3 Ст. ложки</p>
+<p>Соль ---------------------------------------------- 1 Чайная ложка</p>
+<p>Перец черный ---------------------------------------- 1 Чайная ложка</p>
+<p>Картошка ---------------------------------------------- 1 Кг</p>
+<p>Тмин -------------------------------------------- 0,5 Чайных ложки</p>
+<h1 style="color:#CD5C5C">Приготовление</h1>
+
+<p>1. Говядину промойте и нарежьте небольшими кусочками. Выберите жилки и пленочки. </p>
+<p>2. Луковицу и чеснок очистите. Лук нарежьте полукольцами, чеснок измельчите. </p>
+<p>3. Болгарский перец промойте, вырежьте семечки и плодоножки, нарежьте крупными полукольцами.<p> 
+<p>4. Помидоры промойте, вырежьте плодоножки. Если не ленитесь, снимите шкурку, опустив помидор на пару минут в кипяток. Нарежьте крупными кубиками.</p> 
+<p>5. Картошку промойте, очистите, нарежьте крупными кубиками. Отлично в гуляше смотрится маленькая свежая картошечка. Ее даже можно не чистить и не резать, просто хорошенько промыть. </p>
+<p>6. В большой кастрюле с толстыми стенками нагрейте растительное масло или растопите жир.</p>
+<p>7. Выложите в сковороду лук и обжарьте, помешивая, до золотистого цвета. </p>
+<p>8. Уменьшите огонь и всыпьте молотую паприку. Постоянно помешивайте. </p>
+<p>9. Добавьте мясо и перемешайте так, чтобы кусочки мяса были в паприке.</p> 
+<p>10. Обжарьте на медленном огне. Мясо должно приобрести нежно-коричневый оттенок. </p>
+<p>11. Затем добавьте лавровый лист, тмин, соль, перец и измельченный чеснок. </p>
+<p>12. Добавьте воды - столько, чтобы она покрыла ингредиенты. Оставьте тушиться на средне-медленном огне на 30-40 минут.</p> 
+<p>13. По истечению указанного времени добавьте болгарский перец, помидоры и картошку. Добавьте воды, если нужно (столько, чтобы все было ею покрыто) и готовьте еще полчаса. Готовое мясо и овощи должны быть мягкими. </p>
+<p>Гуляш с болгарским перцем готов! </p>
+<p>Подавать гуляш желательно со сметаной, рубленой петрушкой и белым хлебом. Хлеб можно макать в соус и скушать это чудесное блюдо до последней капли.</p> 
+<p>Приятного аппетита!</p>
+<p><img src="http://img.povar.ru/main/3b/d6/56/65/gulyash_s_bolgarskim_percem-66369.JPG">
+
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-header" role="tab" id="headingThree">
+      <h5 class="mb-0">
+        <a class="collapsed" data-toggle="collapse" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+          <h4>Пирожки жареные с капустой</h4>
+        </a>
+      </h5>
+    </div>
+    <div id="collapseThree" class="collapse" role="tabpanel" aria-labelledby="headingThree" data-parent="#accordion">
+      <div class="card-body">
+        <p>Капуста белокочанная  ---- 800-1000 Грамм </p>
+        <p>Лук репчатый  ---- 1 Штука </p>
+        <p>Готовое дрожжевое тесто ---- 1 Килограмм </p>
+        <p>Растительное масло ---- По вкусу </p>
+        <p>Соль ---- По вкусу </p>
+        <p>Перец ---- По вкусу </p>
+        <h1 style="color:#CD5C5C">Приготовление</h1>
+        <p>Нарезаем лук мелкими кубиками.</p>
+        <p>Шинкуем мелко капусту.</p>
+        <p>Разогреваем растительное масло в сковородке и кладем в нее капусту. На небольшом огне жарим ее до полупрозрачного состояния.</p>
+        <p>К прожаренной капусте добавляем лук.</p>
+        <p>Помешивая жарим капусту с луком до полной их готовности, солим и посыпаем перцем по вкусу.</p>
+        <p>Приступаем к лепке пирожков: отделяем небольшой кусок теста и раскатываем его скалкой (можно просто размять руками, я обычно так и делаю). В середину кладем начинку из лука и капусты.
+</p>
+        <p>Соединяем края нашего пирожка очень крепко, чтобы они не расклеились во время жарки.
+</p>
+        <p>На разогретую сковороду выкладываем пирожки швом вниз.</p>
+        <p>Обжариваем со всех сторон. Излишки масла удаляем с помощью бумажной салфетки.</p>
+        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-30" src="http://img.povar.ru/steps/b4/b2/9f/85/pirojki_jarenie_s_kapustoi-69100.jpg" alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-30" src="http://img.povar.ru/steps/94/9b/7a/4a/pirojki_jarenie_s_kapustoi-69101.jpg" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-20" src="http://img.povar.ru/main/29/94/41/b2/pirojki_jarenie_s_kapustoi-69103.jpg" alt="Third slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+    </div>
+  </div>
+</div>
+<button type="button" class="btn btn-secondary">Подписаться на блог!</button>
+</body>
+</html>
